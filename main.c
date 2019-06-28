a05ac19fb2a63ae3497dca2ddf2d4902123333f7
RooD7 <rodrigosoualves7@gmail.com>
2019-06-28 16:35:32 -0300

    correcao entrada dados

--- main.c
+++ main.c
@@ -7,51 +7,59 @@
 #include"ABB.h"
 #define TAM_MAX 100000
 
-int compara(char* ptr, int cont_linha){
-
+int compara(char* ptr, int cont_linha)
+{
 	FILE* arqPalavra = fopen("palavras.txt", "rt");
-	char texto_str2[256];
-	int igual, soma=0, soma2=0;
-	char* ptr2;
-	int cont_linha_palavras_chave;
-	char delim2[] = " ";
+	char texto_str[256];
+	// char* ptr2;
+	// int cont_linha_palavras_chave;
+	// char delim2[] = " ";
 	
-		while(fgets(texto_str2, 256, arqPalavra) != NULL) 
+	int soma=0, soma2=0;
+	while(fgets(texto_str, 256, arqPalavra) != NULL) 
+	{
+		char *pos;
+		if ((pos=strchr(texto_str, '\n')) != NULL)
+			*pos = '\0';
+
+		printf("PAU %s\n", texto_str);
+
+		printf(" COMPARAÇÃO #### %s %s \n",ptr, texto_str );
+
+		soma=0, soma2=0;
+		for (int i = 0; i < strlen(ptr); ++i)
 		{
-			char *ptr2 = strtok(texto_str2, delim2);
-			while(ptr2 != NULL)
+			soma += (ptr[i]);
+			soma2 += (texto_str[i]);
+			if (soma != soma2)
 			{
-				//printf("###### ARQUIVO PALAVRAS %s\n", ptr2);
-				//texto_str2 = strtok(texto_str2, "\n");
-				soma=0, soma2=0;
-
-		    	printf(" #### %s %s \n",ptr, ptr2 );
-
-		    	for (int i = 0; i < strlen(ptr2); ++i)
-				{
-					soma += (ptr2[i]);
-					soma2 += (ptr[i]);
-					printf("%d %d \n",soma,soma2 );
-					if (soma != soma2)
-					{
-						break;
-					}
-				}
-
-		    	//if (((soma == soma2)) && (strlen(ptr) > 4) && (strlen(ptr) <32))
-					printf("soma 1: %d soma 2: %d \n",soma,soma2 );
-		    	if (soma == soma2)
-		    	{	
-		    		printf("FECHOU!\n");			
-					fclose(arqPalavra);
-					return 0;
-		    	}
-		    ptr2 = strtok(NULL, delim2);
-
-		    }
+				break;
+			}
 		}
+		//if (((soma == soma2)) && (strlen(ptr) > 4) && (strlen(ptr) <32))
+			// printf("soma 1: %d soma 2: %d \n",soma,soma2 );
+		if (soma == soma2)
+		{	
+			printf("soma IGUAL!\n");
+			fclose(arqPalavra);
+			return 0;
+		}
+	// 	    ptr2 = strtok(NULL, delim2);
+
+	// 	    }
+	}
 	
 	fclose(arqPalavra);
+	return 1;
+}
+
+void copy_string(char *target, char *source) {
+	while (*source) {
+		*target = *source;
+		source++;
+		target++;
+	}
+	*target = '\0';
 }
 
 int main(int argc, char const *argv[])
@@ -59,7 +67,8 @@ int main(int argc, char const *argv[])
 	char delim[] = " ";
 	//char delim2[] = "\n";
 	char texto_str[256];
-	int	cont_linha = 0;
+	char *ptr = malloc(32 * sizeof(*ptr));
+	char *auxPalavra = malloc(32 * sizeof(*auxPalavra));
 	Lista* lista = criaLISTA();
 	
 	NO *HASH[TAM_MAX];
@@ -76,51 +85,57 @@ int main(int argc, char const *argv[])
 	    exit(1);
 	}
 
+	int	cont_linha = 0;
 	while(fgets(texto_str, 256, arqtexto) != NULL) 
 	{
 
-		char *ptr = strtok(texto_str, delim);
+		printf("##################################################\n");
+		printf("### LINHA: %s\n", texto_str );
+		ptr = strtok(texto_str, delim);
 		cont_linha++;
-		printf("%s \n", texto_str);
-		printf("%d \n", cont_linha);
+		// printf("%s \n", texto_str);33
+		// printf("%d \n", cont_linha);
 		while(ptr != NULL)
 		{
-			printf("####%s \n",ptr );
-			printf("QNTO DEU A FUNCAO DE COMPARA:  %d\n", compara(ptr, cont_linha));
-
-			if(compara(ptr, cont_linha) == 0){
-
-				// insereAVL(raiz, ptr, cont_linha); 
-				// // printf(" INSERIDO: %s - %d\n", ptr, cont_linha );
-				insereLISTA(lista, ptr , cont_linha);
-				// // printf(" INSERIDO: %s - %d\n", ptr, cont_linha );
-				// insereABB(ptr, cont_linha, &raizABB);
-				// insereHASH(ptr,cont_linha, HASH);
-			}
+			// printf("QNTO DEU A FUNCAO DE COMPARA:  %d\n", compara(ptr, cont_linha));
+			copy_string(auxPalavra, ptr);
+			printf("### TEXTO: %s\n", ptr );
 
 			ptr = strtok(NULL, delim);
+			if(compara(auxPalavra, cont_linha) == 0){
+
+				
+				//  insereAVL(raiz, ptr, cont_linha); 
+				// // printf(" INSERIDO: %s - %d\n", ptr, cont_linha );
+				printf("%s - Linha %d\n", auxPalavra, cont_linha);
+				// insereLISTA(lista, auxPalavra , cont_linha);
+				// // printf(" INSERIDO: %s - %d\n", ptr, cont_linha );
+				//  insereABB(auxPalavra, cont_linha, &raizABB);
+				//  insereHASH(auxPalavra,cont_linha, HASH);
+			}
+
 		}
 	}
 	fclose(arqtexto);
 
-	printf("\n########### LISTA ###########\n");
-	exibeLISTA(lista);
+	// printf("\n########### LISTA ###########\n");
+	// // exibeLISTA(lista);
 	
 	printf("\n########### ABB ###########\n");
 	/*ABB*/
-	// insere("banana",9, &raiz);
-	// insereABB("amora",4, &raizABB);
-	// insereABB("couve",3, &raizABB);
-	// insere("banana",3, &raiz);
-	// insere("amora",3, &raiz);
-	// insere("flor",5, &raiz);
-	// insere("vida",6, &raiz);
-	// insere("a",12, &raiz);
+	insereABB("banana",9, &raiz);
+	insereABB("amora",4, &raizABB);
+	insereABB("couve",3, &raizABB);
+	insereABB("banana",3, &raiz);
+	insereABB("amora",3, &raiz);
+	insereABB("flor",5, &raiz);
+	insereABB("vida",6, &raiz);
+	insereABB("a",12, &raiz);
 
 	// printf("\n\n");
 	//imprimeABB(raizABB,0);
 	//imprimeABB(raizABB, igual);
-	//emordemABB(raizABB);
+	emordemABB(raizABB);
 	// imprime(raiz,0);
 	// printf("\n\n");
 	// imprime(raiz,1);
@@ -130,33 +145,33 @@ int main(int argc, char const *argv[])
 	printf("\n############### AVL ##############\n");
 
 
-	// insereAVL(raiz, "rodrigo", 5);
-	// imprimeAVL(raiz, 0);
-	// printf("&*\n");
-	// insere(raiz,"gostoso", 3);
-	// imprimeAVL(raiz, 1);
-	// insere(raiz,"lindo", 1);
-	// imprimeAVL(raiz, 2);
-	// insere(raiz,"lindo", 2);
-	// imprimeAVL(raiz, 3);
-	// insere(raiz,"amor", 1);
-	// imprimeAVL(raiz, 4);
-	// insere(raiz,"amor", 5);
-	// imprimeAVL(raiz, 5);
-	// insere(raiz,"vida", 6);
-	// imprimeAVL(raiz, 6);
-	// insere(raiz,"meu", 1);
-	// imprimeAVL(raiz, 7);
-	// insere(raiz,"amor", 1);
-	// insere(raiz,"gato", 1);
-	// imprimeAVL(raiz, 8);
-	// insere(raiz,"amor", 1);
-	// imprimeAVL(raiz, 9);
-	// printf("#########3%s\n",raiz ->palavra );
-	//exibir_ordenadoAVL(raiz);
+	insereAVL(raiz, "rodrigo", 5);
+	imprimeAVL(raiz, 0);
+	printf("&*\n");
+	insereAVL(raiz,"gostoso", 3);
+	imprimeAVL(raiz, 1);
+	insereAVL(raiz,"lindo", 1);
+	imprimeAVL(raiz, 2);
+	insereAVL(raiz,"lindo", 2);
+	imprimeAVL(raiz, 3);
+	insereAVL(raiz,"amor", 1);
+	imprimeAVL(raiz, 4);
+	insereAVL(raiz,"amor", 5);
+	imprimeAVL(raiz, 5);
+	insereAVL(raiz,"vida", 6);
+	imprimeAVL(raiz, 6);
+	insereAVL(raiz,"meu", 1);
+	imprimeAVL(raiz, 7);
+	insereAVL(raiz,"amor", 1);
+	insereAVL(raiz,"gato", 1);
+	imprimeAVL(raiz, 8);
+	insereAVL(raiz,"amor", 1);
+	imprimeAVL(raiz, 9);
+	printf("#########3%s\n",raiz ->palavra );
+	exibir_ordenadoAVL(raiz);
 
-	 printf("\n #################### HASH ####################\n");
-	//imprimeHASH(HASH);
+	//  printf("\n #################### HASH ####################\n");
+	// imprimeHASH(HASH);
 	printf("\n");
 
 	// insereHASH("rodrigo",3, HASH);
